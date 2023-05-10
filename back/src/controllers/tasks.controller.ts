46b7de175decad472021a1c2ec3c7acd0d0b6a20
Lichy IO <lichy.cloud@gmail.com>
2023-05-10 12:46:39 +0300

    .

--- back/src/controllers/tasks.controller.ts
+++ back/src/controllers/tasks.controller.ts
@@ -1,5 +1,5 @@
 import { Router } from 'express';
-import { readFileSync } from 'fs';
+import { readFileSync, writeFileSync } from 'fs';
 import { Task } from '../types/task.type';
 
 const router = Router();
@@ -8,6 +8,30 @@ router.get('/tasks', async (req, res, next) => {
   try {
     const tasks: Task[] = JSON.parse(readFileSync('./mock/tasks.json').toString());
     res.json(tasks);
+  } catch (error) {
+    next(error);
+  }
+});
+
+router.get('/tasks/:id', async (req, res, next) => {
+  try {
+    const tasks: Task[] = JSON.parse(readFileSync('./mock/tasks.json').toString());
+    const task = tasks.find((task) => task.id === parseInt(req.params.id));
+    res.json(task ? task : { msg: 'Task not found' });
+  } catch (error) {
+    next(error);
+  }
+});
+
+router.post('/tasks', async (req, res, next) => {
+  try {
+    const tasks: Task[] = JSON.parse(readFileSync('./mock/tasks.json').toString());
+    if (req?.body) {
+      tasks.push(req.body);
+      writeFileSync('./mock/tasks.json', JSON.stringify(tasks));
+      return res.json(tasks);
+    }
+    res.json({ msg: 'No data provided' });
   } catch (error) {
     next(error);
   }
